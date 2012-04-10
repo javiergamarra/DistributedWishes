4b4f030f5366a94176d55fc4a0136312c3b48f9d
Javier Gamarra <nhpatt@gmail.com>
2012-04-10 13:13:28 +0200

    0 - Creating project

---
+++ DistributedWishes/src/com/nhpatt/distributedwishes/DistributedWishesActivity.java
@@ -0,0 +1,13 @@
+package com.nhpatt.distributedwishes;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DistributedWishesActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
