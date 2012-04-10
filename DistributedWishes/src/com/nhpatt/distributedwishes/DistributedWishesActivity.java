ef93c19c65482bce18546616bef2e87912f316a0
Javier Gamarra <nhpatt@gmail.com>
2012-04-10 19:31:25 +0200

    7 - Adding string written to list

--- DistributedWishes/src/com/nhpatt/distributedwishes/DistributedWishesActivity.java
+++ DistributedWishes/src/com/nhpatt/distributedwishes/DistributedWishesActivity.java
@@ -1,26 +1,35 @@
 package com.nhpatt.distributedwishes;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class DistributedWishesActivity extends Activity {
 	private static final String TAG = "DISTRIBUTED_WISHES";
+
+	private final List<String> wishes = new ArrayList<String>();
 
 	@Override
 	public void onCreate(final Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
-		Log.e(TAG, "Actividad creada");
+		Log.e(TAG, "Activity created");
 
 		Button button = (Button) findViewById(R.id.saveWish);
 		button.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(final View v) {
-				Log.e(TAG, "Butón pulsado");
+				EditText wishField = (EditText) findViewById(R.id.wish);
+				String wish = wishField.getText().toString();
+				wishes.add(wish);
+				Log.e(TAG, "Added wish: " + wish);
 			}
 		});
 	}
