9e68c22cdf282856beac8ec2c89c20b2303a69e3
Javier Gamarra <nhpatt@gmail.com>
2012-04-10 19:45:24 +0200

    8 - another big jump, adding a listview with simple adapter

--- DistributedWishes/src/com/nhpatt/distributedwishes/DistributedWishesActivity.java
+++ DistributedWishes/src/com/nhpatt/distributedwishes/DistributedWishesActivity.java
@@ -3,24 +3,28 @@ package com.nhpatt.distributedwishes;
 import java.util.ArrayList;
 import java.util.List;
 
-import android.app.Activity;
+import android.app.ListActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListAdapter;
 
-public class DistributedWishesActivity extends Activity {
+public class DistributedWishesActivity extends ListActivity {
 	private static final String TAG = "DISTRIBUTED_WISHES";
 
 	private final List<String> wishes = new ArrayList<String>();
+	private ArrayAdapter<String> adapter;
 
 	@Override
 	public void onCreate(final Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
-		Log.e(TAG, "Activity created");
+		adapter = new ArrayAdapter<String>(this,
+				android.R.layout.simple_list_item_1, wishes);
+		setListAdapter(adapter);
 
 		Button button = (Button) findViewById(R.id.saveWish);
 		button.setOnClickListener(new OnClickListener() {
@@ -29,7 +33,7 @@ public class DistributedWishesActivity extends Activity {
 				EditText wishField = (EditText) findViewById(R.id.wish);
 				String wish = wishField.getText().toString();
 				wishes.add(wish);
-				Log.e(TAG, "Added wish: " + wish);
+				adapter.notifyDataSetChanged();
 			}
 		});
 	}
