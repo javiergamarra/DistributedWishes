bc45f1a0600e90ab556fffcd78f061ff1cf721d5
Javier Gamarra <nhpatt@gmail.com>
2012-04-10 19:27:12 +0200

    6 - jump of faith, adding id to a button, an OnClickListener and logging interaction

--- DistributedWishes/src/com/nhpatt/distributedwishes/DistributedWishesActivity.java
+++ DistributedWishes/src/com/nhpatt/distributedwishes/DistributedWishesActivity.java
@@ -3,12 +3,25 @@ package com.nhpatt.distributedwishes;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class DistributedWishesActivity extends Activity {
+	private static final String TAG = "DISTRIBUTED_WISHES";
+
 	@Override
 	public void onCreate(final Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
-		Log.e("DISTRIBUTED_WISHES", "Actividad creada");
+		Log.e(TAG, "Actividad creada");
+
+		Button button = (Button) findViewById(R.id.saveWish);
+		button.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(final View v) {
+				Log.e(TAG, "Butón pulsado");
+			}
+		});
 	}
 }
